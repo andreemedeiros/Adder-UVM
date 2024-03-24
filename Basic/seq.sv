743e47a4e231f61b092362b1081d5afef024799c
André Medeiros <andre.escariao1@gmail.com>
2024-03-24 02:33:02 -0300

    Update and rename full_adder_seq.sv to seq.sv

--- Basic/seq.sv
+++ Basic/seq.sv
@@ -1,15 +1,15 @@
-class full_adder_seq extends uvm_sequence#(full_adder_seq_item);
-  `uvm_object_utils(full_adder_seq)
-  function new(string name ="full_adder_seq");
+class adder_seq extends uvm_sequence#(adder_seq_item);
+  `uvm_object_utils(adder_seq)
+  function new(string name ="adder_seq");
     super.new(name);
   endfunction
   task body();
-    full_adder_seq_item full_add;
+    adder_seq_item full_add;
     repeat(6)
       begin
         //`uvm_info("Seq loop",UVM_LOW)
         //#25;
-        `uvm_info("","Printing full_adder_seq", UVM_LOW)
+        `uvm_info("","Printing adder_seq", UVM_LOW)
         `uvm_do(req)
         //full_add = full_adder_seq_item::type_id::create("full_add");
         //start_item(full_add);
