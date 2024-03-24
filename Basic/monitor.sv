fb46de18a945894c8c4a41100fbdc40b3e06c713
André Medeiros <andre.escariao1@gmail.com>
2024-03-24 02:29:01 -0300

    Update and rename full_add_monitor.sv to monitor.sv

--- Basic/monitor.sv
+++ Basic/monitor.sv
@@ -1,16 +1,16 @@
-class full_add_monitor extends uvm_monitor;
-  `uvm_component_utils(full_add_monitor)
-  uvm_analysis_port#(full_adder_seq_item) full_add_moni;
+class monitor extends uvm_monitor;
+  `uvm_component_utils(add_monitor)
+  uvm_analysis_port#(adder_seq_item) add_moni;
   virtual fulladder_if vif;
-  full_adder_seq_item collect;
-  function new(string name ="full_add_monitor",uvm_component parent);
+  adder_seq_item collect;
+  function new(string name ="add_monitor",uvm_component parent);
     super.new(name,parent);
     collect = new("collect");
     full_add_moni = new("full_add_moni", this);
   endfunction
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    `uvm_info("","Printing full_add_monitor_build", UVM_LOW)
+    `uvm_info("","Printing add_monitor_build", UVM_LOW)
     $display($time, "build_mon");
     if(!uvm_config_db#(virtual fulladder_if)::get(this, "", "vif", vif))
        `uvm_fatal("NOVIF",{"virtual interface must be set for: ",get_full_name(),".vif"});
